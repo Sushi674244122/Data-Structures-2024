cfffb7f188241a59fcbaab7c4c85789dc381610f
Suwat <suwat.tac@mail.pbru.ac.th>
2024-12-06 10:06:06 +0700

    Update first.py

--- codes/first.py
+++ codes/first.py
@@ -1,5 +1,19 @@
+def fahrenheit_to_celsius(fahrenheit):
+  celsius = (fahrenheit - 32) / 1.8
+  return celsius
+
+def celsius_to_fahrenheit(celsius):
+  fahrenheit = (celsius * 1.8) + 32
+  return fahrenheit
+
 fah = 98.6
-cel = (fah -32) / 1.8
+cel = fahrenheit_to_celsius(fah)
 
 print('Fahrenheit = ', fah)
 print('Celsius = ', cel)
+
+cel = 37
+fah = celsius_to_fahrenheit(cel)
+
+print('Celsius = ', cel)
+print('Fahrenheit = ', fah)
