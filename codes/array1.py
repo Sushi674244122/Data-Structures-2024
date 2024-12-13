6878c524ce3ec4c49adab924e383c7b11b01b161
Suwat <suwat.tac@mail.pbru.ac.th>
2024-12-13 10:59:37 +0700

    Update array1.py

--- codes/array1.py
+++ codes/array1.py
@@ -1,25 +1,19 @@
-cars = ["Ford", "Volvo", "BMW"]
+KEY=2
 
-carlist = list(cars)
-p = cars.index('BMW')
-cars.insert(1,'Toyota')
-
-print(cars)
-print('P = ',p)
-
-number = [ 7, 6 ,4,2 , 9 , 20]
-number.sort(reverse=True)
-print(number)
+def encryption(plainText):
+  cesar =list('    ')
+  for i in range(len(plainText)):
+	  cesar[i] = chr(ord(plainText[i])+KEY)
+  return cesar
+                 
+def decryption(cypherText):
+	decypher = list('    ')
+	for i in range(len(cypherText)):
+		decypher[i] = chr(ord(cypherText[i])-KEY)
+	return decypher
+                 
 
 plainText = 'bird'
-cesar =list('    ')
-decypher = list('    ')
-for i in range(4):
-	cesar[i] = chr(ord(plainText[i])+2)
-    
-for i in range(4):
-	decypher[i] = chr(ord(cesar[i])-2)
 
-print(plainText)
-print(cesar)
-print(decypher)
+print(encryption(plainText))
+print(decryption(encryption(plainText)))
